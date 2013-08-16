ff4beced62d3546306f3c68e6b964885ad39c0f5
Galina Shishova <galina.shishova@chisw.us>
2013-08-16 16:26:31 +0300

    screenshot best

--- gen/com/nichtemna/manystatestoggler/R.java
+++ gen/com/nichtemna/manystatestoggler/R.java
@@ -49,15 +49,11 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int toggle_unchecked_right=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080003;
+        public static final int action_settings=0x7f080001;
         public static final int main_toggle_layout=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int toggle=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
